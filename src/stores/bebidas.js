645066a56b38a750bcfbd9845b743e9aa2276494
Leixhik <eduardo.leijapl@uanl.edu.mx>
2026-01-09 11:23:05 -0600

    Se crearon los Services para la API y los config de Axios, teniendo una mejor estructura por medio de las carpetas lib y services, modificando el onMounted por APIService, sigue modificar el obtenerRecetas

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -1,6 +1,6 @@
 import { ref, reactive, onMounted } from "vue";
 import { defineStore } from "pinia";
-import axios from 'axios'
+import APIService from '../services/APIService'
 
 export const useBebidasStore = defineStore('bebidas', () => {
 
@@ -11,7 +11,7 @@ export const useBebidasStore = defineStore('bebidas', () => {
   })
 
   onMounted(async function() {
-    const {data: {drinks}} = await axios('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+    const {data: {drinks}} = await APIService.obtenerCategorias()
 
     categorias.value = drinks
   })
